2d19605385f7e0aa61c48071a16fc3b86dcfcbc0
Guilherme Borges <borgesg226@gmail.com>
2026-07-17 11:24:26 -0300

    feat: add lib radix-ui and create NewTransactionModal component

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,16 +1,25 @@
 import { HeaderContainer, HeaderContent, NewTransactionButton } from "./styles";
 import LogoImg from "../../assets/LogoImg.svg";
 
+import * as Dialog from '@radix-ui/react-dialog';
+import { NewTransactionModal } from "../NewTransactionModal";
+
 export function Header() {
     return (
         <HeaderContainer>
             <HeaderContent>
                 <img src={LogoImg} alt="dt money" />
                 <h1>DT Money</h1>
-                
-                <NewTransactionButton type="button">
-                    Nova transação
-                </NewTransactionButton>
+
+                <Dialog.Root>
+                    <Dialog.Trigger asChild>
+                        <NewTransactionButton type="button">
+                            Nova transação
+                        </NewTransactionButton>
+                    </Dialog.Trigger>
+
+                    <NewTransactionModal />
+                </Dialog.Root>
             </HeaderContent>
         </HeaderContainer>
     )
---
+++ src/components/NewTransactionModal/index.tsx
@@ -0,0 +1,29 @@
+import * as Dialog from '@radix-ui/react-dialog';
+import { CloseButton, Content, Overlay } from './styles';
+import { X } from 'phosphor-react';
+
+export function NewTransactionModal() {
+    return (
+        <Dialog.Portal>
+            <Overlay />
+
+            <Content>
+                <Dialog.Title>Nova Transação</Dialog.Title>
+
+                <CloseButton>
+                    <X />
+                </CloseButton>
+
+                <form>
+                    <input type="text" placeholder="Descrição" />
+                    <input type="Number" placeholder="Preço" />
+                    <input type="text" placeholder="Categoria" />
+
+                    <button type="submit">
+                        Cadastrar
+                    </button>
+                </form>
+            </Content>
+        </Dialog.Portal>
+    )
+}
